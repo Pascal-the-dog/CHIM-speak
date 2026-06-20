c73adafaa932c24ec162bc7804e3327e37f95a0a
Pascal-the-dog <242273731+Pascal-the-dog@users.noreply.github.com>
2026-06-20 12:11:54 -0400

    Vivec Enhances the Reality of the Earth-Bones

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,6 +1,6 @@
 #[derive(Debug, Clone, PartialEq)]
 pub enum Token {
-    // ... loop tokens remain unchanged ...
+    // ... loop tokens ...
     WheelTurns,
     FromFirstDawn,
     UntilMiddleDawn(i64),
